d0ef40f18bffe2dbd3a2b5c571fae06b50bdc1e4
Teetow <teetow@gmail.com>
2024-12-03 00:57:09 +0100

    tweak CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import preact from "@preact/preset-vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: "/santagen/",
+  build: {
+    outDir: "dist",
+    assetsDir: "",
+  },
 });
